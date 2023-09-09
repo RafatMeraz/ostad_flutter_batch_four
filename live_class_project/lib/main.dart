6a6052e0573d95b95a01221fddaf1d4e3f72caa5
RafatMeraz <razhossain201@gmail.com>
2023-09-09 23:33:46 +0600

    module 6 class 1 - Container - Elevated button - Icon button - Text button - Outlined button - Padding - Sizedbox

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -20,127 +20,144 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        backgroundColor: Colors.purple,
-        centerTitle: true,
         title: Text('Home'),
-        leading: Icon(
-          Icons.adb_rounded,
-          color: Colors.amber,
-          size: 32,
-        ),
       ),
-      // body: Column(
-      //   children: [
-      //     Column(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       crossAxisAlignment: CrossAxisAlignment.end,
-      //       children: [
-      //         Text('Hello world'),
-      //         Text('Hello world'),
-      //         Text('Hello world'),
-      //         Text('Hello world'),
-      //         Text('Hello world'),
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.center,
-      //           crossAxisAlignment: CrossAxisAlignment.center,
-      //           children: [
-      //             Text('hello '),
-      //             Text('world '),
-      //             Text('are '),
-      //             Text('you '),
-      //             Text('listening'),
-      //           ],
-      //         )
-      //       ],
-      //     ),
-      //     Row(
-      //       children: [
-      //         Text('sdfdsf'),
-      //         Text('sdfdsf'),
-      //         Text('sdfdsf'),
-      //         Text('sdfdsf'),
-      //       ],
-      //     )
-      //   ],
-      // ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('Hello '),
-              Text('Class '),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('dsfds')
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('Hello '),
-              Text('Class '),
-            ],
-          ),
-          Image.asset('images/dalli.jpg', width: 120, height: 90, fit: BoxFit.cover,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Image.network('https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
-                width: 200,
-                height: 200,
-                fit: BoxFit.scaleDown,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 100,
+              margin: EdgeInsets.all(16),
+              // padding: EdgeInsets.only(top: 10, bottom: 10, left: 4),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                border: Border.all(color: Colors.black, width: 2),
+                // // borderRadius: BorderRadius.circular(10)
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                ),
+                // shape: BoxShape.circle
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('To register, tap on '),
-              Text('Sign up'),
-            ],
-          ),
-          RichText(
-            text: TextSpan(
-              style: TextStyle(fontSize: 20, color: Colors.purple),
-              children: [
-                TextSpan(
-                    text: 'Hello ', style: TextStyle(color: Colors.purple)),
-                TextSpan(text: 'Name ', style: TextStyle(color: Colors.amber)),
-                TextSpan(
-                    text: 'Is skflds ',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600)),
-              ],
+              child: Text('Hello'),
+              alignment: Alignment.center,
             ),
-          ),
-          RichText(text: TextSpan(
-            text: 'To register, tap on ',
-            children: [
-              TextSpan(
-                text: 'Sign up',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
+            Container(
+              margin: EdgeInsets.all(16),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500
+                  ),
+                  padding: EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                ),
+                onLongPress: () {
+                  print('Email deleted!');
+                },
+                onPressed: () {
+                  print('Email has been sent');
+                },
+                child: Text('Send Email'),
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.purple,
+                textStyle: TextStyle(
+                  fontSize: 24
                 )
-              )
-            ]
-          ))
-        ],
+              ),
+              onLongPress: () {
+                print('Long press');
+              },
+              onPressed: () {
+                print('Resend email');
+              },
+              child: Text('Resend'),
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            OutlinedButton(
+              onLongPress: () {
+                print('Long press on outlined button');
+              },
+              onPressed: () {},
+              child: Text('Outlined button'),
+            ),
+            // input
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                // maxLines: 5,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                decoration: InputDecoration(
+                  fillColor: Colors.purple,
+                  filled: true,
+                  hintText: 'Enter your email address',
+                  hintStyle: TextStyle(
+                    color: Colors.white70
+                  ),
+                  suffixIcon: Icon(Icons.email_outlined, color: Colors.white70,),
+                  prefixIcon: Icon(Icons.email_outlined, color: Colors.white70,),
+                  label: Text('Email address'),
+                  labelStyle: TextStyle(
+                    color: Colors.white
+                  )
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                controller: TextEditingController(),
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                decoration: InputDecoration(
+                  fillColor: Colors.purple,
+                  filled: true,
+                  hintText: 'Enter your password',
+                  hintStyle: TextStyle(
+                    color: Colors.white70
+                  ),
+                  suffixIcon: Icon(Icons.password, color: Colors.white70,),
+                  label: Text('Password'),
+                  labelStyle: TextStyle(
+                    color: Colors.white
+                  ),
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
