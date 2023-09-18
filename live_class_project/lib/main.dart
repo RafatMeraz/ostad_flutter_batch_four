457bb00e0186d0b557a260ff96559d9067a93b9b
RafatMeraz <razhossain201@gmail.com>
2023-09-18 23:37:05 +0600

    theming has been introduced

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-/// Navigation - one page -> another page
-/// Navigator 1(Simple and Lengthy), 2 (complex)
-/// Routers Package -> Getx, GoRouter, Auto Route
-
-/// TODO : Stack, Queue, Navigator, Navigation
+/// TODO : Add text field theme
+/// TODO : Dark mode er jonno theme data add korben
 
 void main() {
   runApp(MyApp());
@@ -17,11 +14,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8)
+            ),
+            elevation: 5,
+            textStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.6,
+              wordSpacing: 0.5
+            )
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            textStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600
+            ),
+            foregroundColor: Colors.deepOrange
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pink,
+          elevation: 5,
+          shadowColor: Colors.green
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 18
+          ),
+          bodySmall: TextStyle(
+            fontSize: 10
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 22
+          ),
+        )
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.amberAccent,
+            elevation: 5,
+            shadowColor: Color(0xFF510622)
+        ),
+      ),
+      themeMode: ThemeMode.dark,
     );
   }
 }
 
-/// Route
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -35,50 +84,19 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Home',
-              style: TextStyle(fontSize: 24),
-            ),
+            Text('Hello world',),
+            Text('Hello world', style: Theme.of(context).textTheme.bodySmall,),
+            Text('Hello world', style: Theme.of(context).textTheme.bodyLarge,),
+            Text('Hello world', style: Theme.of(context).textTheme.headlineMedium,),
+            TextButton(onPressed: () {}, child: Text('Tap here'),),
+            TextButton(onPressed: () {}, child: Text('Tap here'),),
+            TextButton(onPressed: () {}, child: Text('Tap here'),),
             ElevatedButton(
-              onPressed: () {
-                /// Navigation - Route home -> Route settings
-                /// Navigator
-                /// Navigator.typeOfNavigation(currentLocation, Destination);
-                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to settings'),
-            ),
+              onPressed: () {}, child: Text('Tap here'),),
             ElevatedButton(
-              onPressed: () {
-                /// Navigation - Route home -> Route settings
-                /// Navigator
-                /// Navigator.typeOfNavigation(currentLocation, Destination);
-                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => OrdersScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to Orders'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                /// Navigation - Route home -> Route settings
-                /// Navigator
-                /// Navigator.typeOfNavigation(currentLocation, Destination);
-                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
-
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductListScreen()));
-              },
-              child: Text('Go to Product list'),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {},
+              child: Text('Tap here'),
             ),
           ],
         ),
@@ -87,140 +105,6 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-/// Route
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Settings',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                      (route) => false,
-                );
-              },
-              child: Text('Home'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/// Route
-class OrdersScreen extends StatelessWidget {
-  const OrdersScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Orders'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Orders',
-              style: TextStyle(fontSize: 24),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to settings'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back to home'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProductListScreen extends StatelessWidget {
-  const ProductListScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product list'),
-      ),
-      body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return ListTile(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ProductDetailsScreen(productName: index.toString(),
-                  );
-                })).then((value) {
-                  print(value);
-                });
-              },
-              title: Text(index.toString()),
-              subtitle: Text('Product details $index'),
-            );
-          }),
-    );
-  }
-}
-
-class ProductDetailsScreen extends StatelessWidget {
-  final String productName; /// Mandatory/Required
-  final double? price; /// Optional
-  const ProductDetailsScreen({super.key, required this.productName, this.price});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Details'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(productName, style: TextStyle(
-              fontSize: 24
-            ),),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context, 'my-name-$productName');
-              },
-              child: Text('Back'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 
 
