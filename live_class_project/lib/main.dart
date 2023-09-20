c3bcd884ce161d997509233fea804565d7cd412b
RafatMeraz <razhossain201@gmail.com>
2023-09-20 23:32:58 +0600

    dialog and bottom sheet has been added

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: Home(),
     );
   }
@@ -20,35 +20,77 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: Icon(CupertinoIcons.home),
-        middle: Text('Home'),
-        trailing: CupertinoSwitch(
-          onChanged: (bool onChange) {
-            
-          }, value: true,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            CupertinoButton.filled(child: Text('sfd'), onPressed: () {}),
-            CupertinoTextField(),
-            CupertinoTabBar(items: [
-              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
-              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
-              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
-            ])
-          ],
-        ),
+      body: Column(
+        children: [
+          Switch(value: true, onChanged: (bool value) {}),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Alert'),
+                      content: Text('You are in danger'),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel')),
+                        TextButton(
+                            onPressed: () {
+                              // ACTION
+                            },
+                            child: Text('Okay')),
+                      ],
+                    );
+                  });
+            },
+            child: Text('Show dialog'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showAboutDialog(
+                  context: context,
+                  applicationName: "PikaPie",
+                  applicationVersion: '1.0.4',
+                  children: [
+                    Text('This application is good for regular uses!')
+                  ]);
+            },
+            child: Text('show about'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+            showModalBottomSheet(
+              isDismissible: false,
+                backgroundColor: Colors.grey.shade100,
+                barrierColor: Colors.black45,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  )
+                ),
+                context: context, builder: (context) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text('This is bottom sheet'),
+                    )
+                  ],
+                ),
+              );
+            });
+          }, child: Text('show bottom sheet'),),
+        ],
       ),
     );
   }
