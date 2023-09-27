fd8b228b5c5e4ec2e1320b5b0991672863bdd9fa
RafatMeraz <razhossain201@gmail.com>
2023-09-27 22:57:46 +0600

    responsive design - Stack - Align - Positioned

--- live_class_project/lib/home_screen.dart
+++ live_class_project/lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// MediaQuery
+/// Stack, Positioned, Align
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,111 +11,57 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        children: [
-          /// Ratio := Width : height
-          /// Ratio := 16/16
-          /// 100 : 100
-          // AspectRatio(
-          //   aspectRatio: 16/4,
-          //   child: Container(
-          //     color: Colors.red,
-          //     child: Image.network(
-          //         'https://images.idgesg.net/images/article/2017/10/wireless_network_internet_of_things_iot_thinkstock_853701554_3x2-100740688-large.jpg?auto=webp&quality=85,70',
-          //     fit: BoxFit.cover,
-          //     ),
-          //   ),
-          // ),
-          // Container(
-          //   width: MediaQuery.sizeOf(context).width,
-          //   height: MediaQuery.sizeOf(context).width,
-          //   color: Colors.amberAccent,
-          //   child: FractionallySizedBox(
-          //     widthFactor: 0.5, // 0 - 1
-          //     heightFactor: 0.5,
-          //     child: Container(
-          //       color: Colors.red,
-          //     ),
-          //   ),
-          // )
-          // Flexible(
-          //   fit: FlexFit.tight,
-          //   flex: 3,
-          //   child: Container(
-          //     color: Colors.amber,
-          //     width: MediaQuery.sizeOf(context).width,
-          //   ),
-          // ),
-          // Flexible(
-          //   flex: 7,
-          //   child: Column(
-          //     children: [
-          //       Flexible(
-          //         child: Container(
-          //           color: Colors.red,
-          //           width: MediaQuery.sizeOf(context).width,
-          //         ),
-          //       ),
-          //       SizedBox(height: 50,),
-          //     ],
-          //   ),
-          // ),
-          // Flexible(
-          //   flex: 1,
-          //   child: Row(
-          //     children: [
-          //       Flexible(
-          //         child: Container(
-          //           color: Colors.blue,
-          //         ),
-          //       ),
-          //       Flexible(
-          //         flex: 5,
-          //         child: Container(
-          //           color: Colors.lightGreenAccent,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // Flexible(
-          //   fit: FlexFit.tight,
-          //   flex: 5,
-          //   child: Container(
-          //     color: Colors.purple,
-          //     width: MediaQuery.sizeOf(context).width,
-          //   ),
-          // ),
-          Container(
-            color: Colors.black54,
-            height: 100,
-            width: MediaQuery.sizeOf(context).width,
-          ),
-          // ** Expanded, Flexible
-          Expanded(
-            flex: 10,
-            child: Container(
-              color: Colors.lightGreenAccent,
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: 100,
-                height: 20,
-                child: FittedBox(
-                  child: Text(
-                    ' sa;foksd flsdkf lsdkl;sadsa aksdlfk l;sdkf dlf kl;',
-                  ),
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.pink,
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      color: Colors.green,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              color: Colors.pink,
+            Positioned(
+              bottom: 15,
+              left: 10,
+              child: Container(
+                width: 30,
+                height: 30,
+                color: Colors.black54,
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                width: 20,
+                height: 20,
+                color: Colors.purple,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
