1a162477ce3aa3a42f422de3d6fd11ecb77ea21c
RafatMeraz <razhossain201@gmail.com>
2023-12-16 00:19:26 +0600

    Add firebase push notification

--- live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
+import firebase_messaging
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
 }
